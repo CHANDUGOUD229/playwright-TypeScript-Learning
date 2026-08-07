a5c08e5587197b4c1b75426dba04842769ce5e52
chandra <chandu96qa@gmail.com>
2026-08-07 12:22:39 +0530

    added annotations

--- tests/hooks.spec.ts
+++ tests/hooks.spec.ts
@@ -2,6 +2,14 @@ import { test, expect, Locator, Page, chromium, FrameLocator, firefox, webkit } 
 
 let page: Page;
 
+/*Annotations
+   skip
+   only
+   fixme still is under progress same to skip the test
+   fail
+   slow
+*/
+
 
 test.beforeAll("open App", async ({ browser }) => {
     page = await browser.newPage();
@@ -12,7 +20,7 @@ test.beforeAll("open App", async ({ browser }) => {
 test.afterAll("close the browser", async () => {
     await page.close();
 })
-
+/*
 test.beforeEach("login App", async () => {
     await page.getByRole('link', { name: 'Login' }).click();
     await page.getByRole('textbox', { name: 'Email ID / Username' }).fill('chandu96qa@outlook.com');
@@ -28,6 +36,8 @@ test.afterEach("Logout App", async () => {
     await expect(page.getByRole('link', { name: 'Naukri.com' }).first()).toBeVisible();
 
 })
+
+*/
 
 test.describe("myGroup", async () => {
 
@@ -46,15 +56,40 @@ test.describe("myGroup", async () => {
 
 })
 
-// test("view profile", async () => {
-//     await page.getByRole('link', { name: 'View profile' }).click();
+test("view profile", async ({page,browserName}) => {
+    test.skip(browserName==="chromium","test is skipped browsername firfox")
+    await page.getByRole('link', { name: 'View profile' }).click();
 
-// })
+})
 
-// test("edit option", async () => {
-//     await expect(page.getByRole('link', { name: 'View profile' })).toBeVisible();
-//     await page.getByRole('link', { name: 'View profile' }).dblclick();
-//     await expect(page.getByText('Selenium WebDriverTestngBDD')).toBeVisible();
-//     // await page.locator('#lazyKeySkills').getByText('editOneTheme').dblclick();
+test.skip("edit option", async () => {
+    await expect(page.getByRole('link', { name: 'View profile' })).toBeVisible();
+    await page.getByRole('link', { name: 'View profile' }).dblclick();
+    await expect(page.getByText('Selenium WebDriverTestngBDD')).toBeVisible();
+    // await page.locator('#lazyKeySkills').getByText('editOneTheme').dblclick();
 
-// })
+})
+
+test("launch app store", async ({page,browserName}) => {
+    test.skip(browserName==="chromium","test is skipped browsername firfox")
+    await page.goto("https://www.demoblaze.com/cart.html#");
+})
+
+test.fixme("launch google", async ({page,browserName}) => {
+        await page.goto("https://www.google.com/");
+
+})
+
+test.fail("launch insta", async ({page,browserName}) => {
+        await page.goto("https://www.instagram.com/");
+
+
+})
+
+
+test("launch insta for slow", async ({page,browserName}) => {
+    test.slow(); // triple the default timeout but its not recomended
+        await page.goto("https://www.instagram.com/");
+
+
+})
