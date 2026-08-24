6b120bbcc8b1a42e5f035e73d179dd00517a3bfc
chandra <chandu96qa@gmail.com>
2026-08-24 19:52:50 +0530

    added popup handle and handle authenticaotion thorw API request most Important one

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,10 +6,18 @@ export default defineConfig({
   expect:{timeout:40*1000},
   reporter:'html',
   use:{
+
+    actionTimeout:10*10000,//it will wait each and evvery action
+    navigationTimeout:10*10000, //it will wait for navigaition
     browserName:'chromium',
-    headless:true
+    headless:true,
+    screenshot:'only-on-failure',
+    video:'retain-on-failure',
+    trace:'retain-on-failure'
 
   }
   
 });
+
+
 
